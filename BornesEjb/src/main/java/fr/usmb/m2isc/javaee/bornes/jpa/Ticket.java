d79fdc034d7b0d11bdd18e196edbe2cb5999495c
Huseyin Yurtseven <yurtseven68210@hotmail.fr>
2022-10-26 12:23:52 +0200

    add methode in Ticket class

--- BornesEjb/src/main/java/fr/usmb/m2isc/javaee/bornes/jpa/Ticket.java
+++ BornesEjb/src/main/java/fr/usmb/m2isc/javaee/bornes/jpa/Ticket.java
@@ -8,6 +8,9 @@ public class Ticket {
     private UUID id;
     private Date dateEntree;
     private Date dateSortie;
+    private Paiement[] paiment_list;
+
+
 
     // un constructeur par défaut
     public Ticket() {
@@ -42,6 +45,71 @@ public class Ticket {
 
     public void setDateSortie(Date dateSortie) {
         this.dateSortie = dateSortie;
+    }
+
+    //add new paiement in array
+    public  Paiement[] addPaiement(int n, Paiement x)
+   {
+       int i;
+   
+       // create a new array of size n+1
+       Paiement newarr[] = new Paiement[n + 1];
+   
+       // insert the elements from
+       // the old array into the new array
+       // insert all elements till n
+       // then insert x at n+1
+       for (i = 0; i < n; i++)
+           newarr[i] = this.paiment_list[i];
+   
+       newarr[n] = x;
+
+       return newarr;
+   }
+
+   //total of all paiement
+    public double totalPaiement(){
+         double total = 0;
+         for (int i = 0; i < this.paiment_list.length; i++) {
+              total += this.paiment_list[i].getMontant();
+         }
+         return total;
+    }
+
+    //remove paiement from array who is given in parameter
+    public Paiement[] removePaiement(int n, Paiement x)
+    {
+        int i;
+        int j = 0;
+   
+        // create a new array of size n-1
+        Paiement newarr[] = new Paiement[n - 1];
+   
+        // insert the elements from
+        // the old array into the new array
+        // except the element at index x
+        for (i = 0; i < n; i++) {
+            if (this.paiment_list[i] != x) {
+                newarr[j++] = this.paiment_list[i];
+            }
+        }
+   
+        return newarr;
+    }
+
+    //return last paiement
+    public Paiement lastPaiement(){
+        return this.paiment_list[this.paiment_list.length - 1];
+    }
+
+    //return paiement from array who is given in parameter
+    public Paiement getPaiement(int n, Paiement x){
+        for (int i = 0; i < n; i++) {
+            if (this.paiment_list[i] == x) {
+                return this.paiment_list[i];
+            }
+        }
+        return null;
     }
 
     // une méthode toDate
